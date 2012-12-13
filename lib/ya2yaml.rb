a34217e055cb8c4ff948acff312d41211034b086
Henrik N <henrik@nyh.se>
2012-12-13 22:54:45 +0100

    Remove ya2yaml KCODE warning in Ruby 1.9+.
    Thanks to @aripollak.

--- lib/ya2yaml.rb
+++ lib/ya2yaml.rb
@@ -25,7 +25,7 @@ class Ya2YAML
 	end
 
 	def _ya2yaml(obj)
-		throw 'set $KCODE to "UTF8".' if $KCODE != 'UTF8'
+		throw 'set $KCODE to "UTF8".' if $KCODE != 'UTF8' && RUBY_VERSION < '1.9'
 		'--- ' + emit(obj,1) + "\n"
 	end
 
